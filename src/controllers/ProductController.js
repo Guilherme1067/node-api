c225cc57f3a38c68a7ef680b4de6e51af78c3c0c
Guilherme <guilherme1067@hotmail.com>
2020-10-03 18:21:41 -0300

    created update,delete and getbyid routes and methods

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const mongoose = require('mongoose');
 
 const Products = mongoose.model('Products')
@@ -8,14 +9,26 @@ module.exports = {
         res.json(products);
     },
 
+     async getById(req,res){
+         const product = await Products.findById(req.params.id);
+         return res.json(product);
+     },
 
+     async update(req,res){
+         const product = await Products.findByIdAndUpdate(req.params.id, req.body)
+        return res.json(product);
+     },
 
+     async delete(req,res){
+        const product = await Products.findByIdAndDelete(req.params.id)
+        
+        return res.send();
+     },
+   
     //req contem todas dados da requisição.
         async store(req,res){
             const product = Products.create(req.body)
             return res.json(product)
         }
-
-    
 };
 
