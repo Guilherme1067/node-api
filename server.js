db0d71fb4c775f4c263c7f74acbd64174fe75687
Guilherme <guilherme1067@hotmail.com>
2020-10-04 10:58:23 -0300

    added cors

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express    = require('express');
+const mongoose   = require('mongoose');
 const requireDir = require('require-dir');
+const cors       = require('cors');
 
 // Iniciando o App
 const app = express();
-
 // Permite o envio de Jsons para a api.
 app.use(express.json())
-
+/*
+Cors: Cross-origin Resource Sharing
+Liberando Acesso de outros domínios à api.
+*/
+app.use(cors());
 // Iniciando o DB
-mongoose.connect("mongodb://localhost:27017/node-api-own",
-{
-    useNewUrlParser: true,
+mongoose.connect("mongodb://localhost:27017/node-api-own", {
+    useNewUrlParser   : true,
     useUnifiedTopology: true
 })
 
